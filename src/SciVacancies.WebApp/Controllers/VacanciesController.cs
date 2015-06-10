2d62e7e635afff2468527f906a9f414ddb50a432
ntemnikov <ntemnikov@alt-lan.com>
2015-06-10 16:53:28 +0300

    added: - Vacancies list work started

--- src/SciVacancies.WebApp/Controllers/VacanciesController.cs
+++ src/SciVacancies.WebApp/Controllers/VacanciesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
@@ -8,6 +9,23 @@ namespace SciVacancies.WebApp.Controllers
 {
     public class VacanciesController : Controller
     {
-        public ViewResult Search() => View();
+
+        public ViewResult Search(
+            List<int> salaries,
+            List<int> contestStates,
+            
+            int period = 0 /* 0=all | 1=1 day |  7=7 days | 30=30 days */,
+            string orderBy = "date" /* count */, 
+            bool orderDescending = true,
+            int pageSize = 10,
+            int page = 1 
+            )
+        {
+
+            return View();
+        }
+
+
+        public ViewResult Details(string id) => View();
     }
 }
