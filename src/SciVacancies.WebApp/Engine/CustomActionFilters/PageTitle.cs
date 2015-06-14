03eb8022d6515a4aa9c0981d40be962627a1b54b
ntemnikov <ntemnikov@alt-lan.com>
2015-06-15 01:19:45 +0300

    added: - comments

--- src/SciVacancies.WebApp/Engine/CustomActionFilters/PageTitle.cs
+++ src/SciVacancies.WebApp/Engine/CustomActionFilters/PageTitle.cs
@@ -2,10 +2,16 @@
 
 namespace SciVacancies.WebApp
 {
+    /// <summary>
+    /// This attribute will set ViewBag.Title property
+    /// </summary>
     public class PageTitle: ActionFilterAttribute
     {
         public string Title;
-
+        /// <summary>
+        /// This attribute will set ViewBag.Title property
+        /// </summary>
+        /// <param name="title"></param>
         public PageTitle(string title)
         {
             Title = title;
