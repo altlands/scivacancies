2d0845d4cefa703a5218e5fff8917558582f4958
ntemnikov <ntemnikov@alt-lan.com>
2015-06-15 01:20:50 +0300

    removed: - unused constructor and property
    fixed:
    - comment

--- src/SciVacancies.WebApp/Engine/CustomAttribute/SiblingPageAttribute.cs
+++ src/SciVacancies.WebApp/Engine/CustomAttribute/SiblingPageAttribute.cs
@@ -3,18 +3,11 @@
 namespace SciVacancies.WebApp.Engine.CustomAttribute
 {
     /// <summary>
-    /// Методы в контроллере, помеченные этим атрибутом, можо собрать, например, в навигационную группу
+    /// Методы в контроллере, помеченные этим атрибутом, можно собрать, например, в навигационную группу.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class SiblingPageAttribute : Attribute
     {
-        public SiblingPageAttribute() { }
-
-        public SiblingPageAttribute(string navigationTitle)
-        {
-            NavigationTitle = navigationTitle;
-        }
-        public string NavigationTitle { get; set; }
     }
 }
 
