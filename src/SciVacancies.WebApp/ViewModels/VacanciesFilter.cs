dd9506903d1629c65034787d6d6df1b1b901b2da
ntemnikov <ntemnikov@alt-lan.com>
2015-06-15 01:15:46 +0300

    added: - VacanciesFilter classes, used in filtering procces in Controller and in displaying filtering parameters in View

---
+++ src/SciVacancies.WebApp/ViewModels/VacanciesFilter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace SciVacancies.WebApp.ViewModels
+{
+
+    public class VacanciesFilter
+    {
+        public VacanciesFilter()
+        {
+            Regions = new List<string>();
+        }
+        public IEnumerable<string> Salaries { get; set; }
+        public IEnumerable<string> ContestStates { get; set; }
+        public IEnumerable<string> Regions { get; set; }
+
+        public int Period { get; set; }
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+
+        private string _orderBy;
+        private string _search;
+
+        public string OrderBy
+        {
+            get
+            {
+                _orderBy = string.IsNullOrWhiteSpace(_orderBy) ? "date_descending" : _orderBy.ToLower();
+                return _orderBy;
+            }
+            set { _orderBy = value; }
+        }
+
+        public string Search
+        {
+            get { return _search; }
+            set
+            {
+                _search = string.IsNullOrWhiteSpace(value) ? null : value;
+            }
+        }
+
+        public void ValidateValues()
+        {
+            if (PageSize < 1) PageSize = 10;
+            if (PageNumber < 1) PageNumber = 1;
+        }
+    }
+}
