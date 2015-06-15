93bfe46f46c6ff7b3272bffe2858cdae4c0749fe
ntemnikov <ntemnikov@alt-lan.com>
2015-06-15 10:59:40 +0300

    deprecated: - Contest renamed to Vacancies. Contest entity is not exists.

--- src/SciVacancies.WebApp/Controllers/VacanciesController.cs
+++ src/SciVacancies.WebApp/Controllers/VacanciesController.cs
@@ -2,8 +2,15 @@
 
 namespace SciVacancies.WebApp.Controllers
 {
+    /// <summary>
+    /// страница с вакансиями (конкурсами)
+    /// </summary>
     public class VacanciesController : Controller
     {
+
         public ViewResult Details(string id) => View();
+
+        public ViewResult Closed() =>View();
+
     }
 }
