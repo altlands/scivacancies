524e7c7ea717de394f8d200677c7792187017007
vladbars <vladbars@gmail.com>
2015-06-16 15:17:39 +0300

    PositionDataModel

--- src/SciVacancies.Domain.Events/DataModels/PositionDataModel.cs
+++ src/SciVacancies.Domain.Events/DataModels/PositionDataModel.cs
@@ -7,6 +7,36 @@ namespace SciVacancies.Domain.Events
 {
     public class PositionDataModel
     {
+        /// <summary>
+        /// Должность
+        /// </summary>
+        public string Name { get;set;}
 
+
+        /// <summary>
+        /// Должность (Полное наименование)
+        /// </summary>
+        public string FullName { get; set; }
+
+
+        /// <summary>
+        /// Отрасль науки
+        /// </summary>
+        public string FieldOfScience { get; set; }
+
+        /// <summary>
+        /// Тематика исследований
+        /// </summary>
+        public string ResearchTheme { get; set; }
+
+        /// <summary>
+        /// Задачи
+        /// </summary>
+        public string Tasks { get; set; }
+
+        /// <summary>
+        /// Критерии оценки
+        /// </summary>
+        public string Criteria { get; set; }
     }
 }
