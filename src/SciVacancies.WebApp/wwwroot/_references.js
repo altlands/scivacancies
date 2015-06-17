ee1c07158d6784a0acc1497ee3fca6a94650e6fe
vladbars <vladbars@gmail.com>
2015-06-17 17:14:07 +0300

    contractType

--- src/SciVacancies.WebApp/wwwroot/_references.js
+++ src/SciVacancies.WebApp/wwwroot/_references.js
@@ -5,6 +5,21 @@
 /// <reference path="js/jquery.tipTip.js" />
 /// <reference path="js/jScrollPane.js" />
 /// <reference path="js/script.js" />
+<<<<<<< .merge_file_a08908
+/// <reference path="lib/hammer.js/hammer.js" />
+/// <reference path="lib/bootstrap-touch-carousel/js/bootstrap-touch-carousel.js" />
+/// <reference path="lib/bootstrap/js/bootstrap.js" />
+/// <reference path="lib/jquery/jquery-migrate.js" />
+/// <reference path="lib/jquery/jquery.js" />
+/// <reference path="lib/jquery-validation/jquery.validate.js" />
+/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+=======
+/// <reference path="js/cusel.js" />
+/// <reference path="js/jquery.bxslider.min.js" />
+/// <reference path="js/jquery.tipTip.js" />
+/// <reference path="js/jScrollPane.js" />
+/// <reference path="js/script.js" />
+<<<<<<< HEAD
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="lib/jquery-validation/jquery.validate.js" />
 /// <reference path="lib/jquery/jquery.js" />
@@ -12,3 +27,6 @@
 /// <reference path="lib/hammer.js/hammer.js" />
 /// <reference path="lib/bootstrap-touch-carousel/js/bootstrap-touch-carousel.js" />
 /// <reference path="lib/bootstrap/js/bootstrap.js" />
+=======
+>>>>>>> .merge_file_a02992
+>>>>>>> vlad
