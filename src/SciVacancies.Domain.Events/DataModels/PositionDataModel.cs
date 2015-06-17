7836c6e7ede567abe8aa5f5eb9db22ab17a60674
vladbars <vladbars@gmail.com>
2015-06-17 17:09:32 +0300

    position vacancy

--- src/SciVacancies.Domain.Events/DataModels/PositionDataModel.cs
+++ src/SciVacancies.Domain.Events/DataModels/PositionDataModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SciVacancies.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -50,19 +51,55 @@ namespace SciVacancies.Domain.Events
         /// </summary>
         public string Bonuses { get; set; }
 
-        /// <summary>
-        /// Дополнительно
-        /// </summary>
-        public string Additional { get; set; }
 
         /// <summary>
         /// Дополнительно
         /// </summary>
         public ContractType ContractType { get; set; }
 
+        /// <summary>
+        /// Социальный пакет
+        /// </summary>
+        public bool SocialPackage { get; set; }
+
+        /// <summary>
+        /// Найм жилья
+        /// </summary>
+        public bool Rent{ get; set; }
+
+        /// <summary>
+        /// Служебное жильё
+        /// </summary>
+        public bool OfficeAccomodation { get; set; }
+
+        /// <summary>
+        /// Компенсация транспорта
+        /// </summary>
+        public bool TransportCompensation { get; set; }
+
+        /// <summary>
+        /// Регион
+        /// </summary>
+        public int RegionId { get; set; }
+
+        /// <summary>
+        /// Населенный пункт 
+        /// </summary>
+        public string CityName { get; set; }
+
+        /// <summary>
+        /// Дополнительно
+        /// </summary>
+        public string Details { get; set; }
 
 
-
+        /// <summary>
+        /// Лицо для получения дополнительных справок
+        /// </summary>
+        public string ContactName { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactDetails { get; set; }
 
     }
 }
