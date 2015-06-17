0764cccf5082795c2c443804eee535b0366ad661
SashaOzz <sashaozz@outlook.com>
2015-06-17 12:42:32 +0300

    Added ASPNET Identity code. DB schema will be a liitle bit later

---
+++ src/SciVacancies.WebApp/Infrastructure/MssqlDbConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SciVacancies.WebApp.Infrastructure
+{
+    public class MssqlDbConfiguration : DbConfiguration
+    {
+        public MssqlDbConfiguration()
+        {           
+            SetDefaultConnectionFactory(new SqlConnectionFactory());
+        }
+    }
+}
---
+++ src/SciVacancies.WebApp/Infrastructure/SciVacUserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SciVacancies.WebApp.Infrastructure
+{
+    [DbConfigurationType(typeof(MssqlDbConfiguration))]
+    public class SciVacUserDbContext : IdentityDbContext<SciVacUser>
+    {
+        public SciVacUserDbContext(string connectionString) : base(connectionString, throwIfV1Schema: false)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
