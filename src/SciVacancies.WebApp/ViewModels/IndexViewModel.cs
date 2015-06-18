2ca094361564b20340de3faaf9a728918101ce4a
ntemnikov <ntemnikov@alt-lan.com>
2015-06-18 11:25:03 +0300

    add untracked files

---
+++ src/SciVacancies.WebApp/ViewModels/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NPoco;
+using SciVacancies.ReadModel.Core;
+
+namespace SciVacancies.WebApp.ViewModels
+{
+    /// <summary>
+    /// Данные для отображения на главной странице сайта
+    /// </summary>
+    public class IndexViewModel
+    {
+
+        public Page<Organization> OrganizationsList{ get; set; }
+    }
+}
