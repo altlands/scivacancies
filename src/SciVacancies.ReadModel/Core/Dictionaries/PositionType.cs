0d1ea3a68a9f1eda8f5d8c36c14bcfd6a67a5c2d
vladbars <vladbars@gmail.com>
2015-06-19 15:47:09 +0300

    PositionType

---
+++ src/SciVacancies.ReadModel/Core/Dictionaries/PositionType.cs
@@ -0,0 +1,20 @@
+﻿using SciVacancies.Domain.Enums;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using NPoco;
+
+namespace SciVacancies.ReadModel.Core
+{
+    [TableName("PositionType")]
+    [PrimaryKey("Guid", AutoIncrement = false)]
+    public class PositionType : BaseEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+    }
+}
