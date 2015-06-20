4107fa5a56e269f0bc8306e43f0b7a996eeaec9c
ntemnikov <ntemnikov@alt-lan.com>
2015-06-20 21:18:07 +0300

    config changes

--- src/SciVacancies.PostgresqlDB/update_ntemnikov.bat
+++ src/SciVacancies.PostgresqlDB/update_ntemnikov.bat
@@ -1,3 +1,3 @@
-rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=sa" -vf version.txt -env=NTEMNIKOV
+rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=postgres" -vf version.txt -env=NTEMNIKOV
 echo Press ENTER to close the WINDOW
 pause > nul
