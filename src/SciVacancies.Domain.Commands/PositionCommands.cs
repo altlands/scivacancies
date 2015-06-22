2139edd1c96f1cf440673dd90734740da9a1a702
ADAvdeev <aavdeev@alt-lan.com>
2015-06-22 10:15:20 +0300

    Commands and Queries - initial

---
+++ src/SciVacancies.Domain.Commands/PositionCommands.cs
@@ -0,0 +1,33 @@
+﻿using SciVacancies.Domain.DataModels;
+
+using System;
+
+using MediatR;
+
+namespace SciVacancies.Domain.Commands
+{
+    public class CreatePosition : CommandBase, IRequest<Guid>
+    {
+        public CreatePosition() : base() { }
+
+        public Guid OrganizationGuid { get; set; }
+
+        public PositionDataModel Data { get; set; }
+    }
+    public class UpdatePosition : CommandBase, IRequest
+    {
+        public UpdatePosition() : base() { }
+
+        public Guid OrganizationGuid { get; set; }
+        public Guid PositionGuid { get; set; }
+
+        public PositionDataModel Data { get; set; }
+    }
+    public class RemovePosition : CommandBase, IRequest
+    {
+        public RemovePosition() : base() { }
+
+        public Guid OrganizationGuid { get; set; }
+        public Guid PositionGuid { get; set; }
+    }
+}
---
+++ src/SciVacancies.Domain.Commands/ResearcherCommands.cs
@@ -0,0 +1,29 @@
+﻿using SciVacancies.Domain.DataModels;
+
+using System;
+
+using MediatR;
+
+namespace SciVacancies.Domain.Commands
+{
+    public class CreateResearcher : CommandBase, IRequest<Guid>
+    {
+        public CreateResearcher() : base() { }
+
+        public ResearcherDataModel Data { get; set; }
+    }
+    public class UpdateResearcher : CommandBase, IRequest
+    {
+        public UpdateResearcher() : base() { }
+
+        public Guid ResearcherGuid { get; set; }
+
+        public ResearcherDataModel Data { get; set; }
+    }
+    public class RemoveResearcher : CommandBase, IRequest
+    {
+        public RemoveResearcher() : base() { }
+
+        public Guid ResearcherGuid { get; set; }
+    }
+}
