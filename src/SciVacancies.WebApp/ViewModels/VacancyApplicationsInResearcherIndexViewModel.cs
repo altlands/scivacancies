430306859eb12f48ce899ffa4a15f79c6b5229cc
ntemnikov <ntemnikov@alt-lan.com>
2015-06-22 11:07:03 +0300

    show in researchers/application my applications

---
+++ src/SciVacancies.WebApp/ViewModels/VacancyApplicationsInResearcherIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using SciVacancies.ReadModel.Core;
+using SciVacancies.WebApp.ViewModels.Base;
+
+namespace SciVacancies.WebApp.ViewModels
+{
+    public class VacancyApplicationsInResearcherIndexViewModel : PageViewModelBase
+    {
+        public VacancyApplicationsInResearcherIndexViewModel()
+        {
+            Title = "Личная карточка пользователя";
+        }
+        public IEnumerable<VacancyApplication> Vacancies { get; set; }
+    }
+}
