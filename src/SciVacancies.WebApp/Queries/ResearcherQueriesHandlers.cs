2dbf4e98b643debf3e4cb6a4470aaa847023f931
ADAvdeev <aavdeev@alt-lan.com>
2015-06-24 13:38:38 +0300

    QueryHandlers were included

---
+++ src/SciVacancies.WebApp/Queries/ResearcherQueriesHandlers.cs
@@ -0,0 +1,28 @@
+﻿using SciVacancies.ReadModel.Core;
+
+using System;
+
+using NPoco;
+using MediatR;
+
+namespace SciVacancies.WebApp.Queries
+{
+    public class SingleResearcherQueryHandler : IRequestHandler<SingleResearcherQuery, Researcher>
+    {
+        private readonly IDatabase _db;
+
+        public SingleResearcherQueryHandler(IDatabase db)
+        {
+            _db = db;
+        }
+
+        public Researcher Handle(SingleResearcherQuery message)
+        {
+            if (message.ResearcherGuid == Guid.Empty) throw new ArgumentNullException($"ResearcherGuid is empty: {message.ResearcherGuid}");
+
+            Researcher researcher = _db.SingleOrDefaultById<Researcher>(message.ResearcherGuid);
+
+            return researcher;
+        }
+    }
+}
---
+++ src/SciVacancies.WebApp/Queries/VacancyApplicationQueriesHandlers.cs
@@ -0,0 +1,84 @@
+﻿using SciVacancies.ReadModel.Core;
+
+using System;
+
+using NPoco;
+using MediatR;
+
+namespace SciVacancies.WebApp.Queries
+{
+    public class SingleVacancyApplicationQueryHandler : IRequestHandler<SingleVacancyApplicationQuery, VacancyApplication>
+    {
+        private readonly IDatabase _db;
+
+        public SingleVacancyApplicationQueryHandler(IDatabase db)
+        {
+            _db = db;
+        }
+
+        public VacancyApplication Handle(SingleVacancyApplicationQuery message)
+        {
+            if (message.VacancyApplicationGuid == Guid.Empty) throw new ArgumentNullException($"VacancyApplicationGuid is empty: {message.VacancyApplicationGuid}");
+
+            VacancyApplication vacancyApplication = _db.SingleOrDefaultById<VacancyApplication>(message.VacancyApplicationGuid);
+
+            return vacancyApplication;
+        }
+    }
+    public class SelectPagedVacancyApplicationsByResearcherQueryHandler : IRequestHandler<SelectPagedVacancyApplicationsByResearcherQuery, Page<VacancyApplication>>
+    {
+        private readonly IDatabase _db;
+
+        public SelectPagedVacancyApplicationsByResearcherQueryHandler(IDatabase db)
+        {
+            _db = db;
+        }
+
+        public Page<VacancyApplication> Handle(SelectPagedVacancyApplicationsByResearcherQuery message)
+        {
+            if (message.ResearcherGuid == Guid.Empty) throw new ArgumentNullException($"ResearcherGuid is empty: {message.ResearcherGuid}");
+
+            Page<VacancyApplication> vacancyApplications = _db.Page<VacancyApplication>(message.PageIndex, message.PageSize, new Sql("SELECT va.* FROM \"VacancyApplications\" va ORDER BY va.\"Guid\" DESC"));
+
+            return vacancyApplications;
+        }
+    }
+    public class SelectPagedVacancyApplicationsByVacancyQueryHandler : IRequestHandler<SelectPagedVacancyApplicationsByVacancyQuery, Page<VacancyApplication>>
+    {
+        private readonly IDatabase _db;
+
+        public SelectPagedVacancyApplicationsByVacancyQueryHandler(IDatabase db)
+        {
+            _db = db;
+        }
+
+        public Page<VacancyApplication> Handle(SelectPagedVacancyApplicationsByVacancyQuery message)
+        {
+            if (message.VacancyGuid == Guid.Empty) throw new ArgumentNullException($"VacancyGuid is empty: {message.VacancyGuid}");
+
+            Page<VacancyApplication> vacancyApplications = _db.Page<VacancyApplication>(message.PageIndex, message.PageSize, new Sql("SELECT va.* FROM \"VacancyApplications\" va ORDER BY va.\"Guid\" DESC"));
+
+            return vacancyApplications;
+        }
+    }
+}
+
+
+
+
+//public class SelectPagedOrganizationsQueryHandler : IRequestHandler<SelectPagedOrganizationsQuery, Page<Organization>>
+//{
+//    private readonly IDatabase _db;
+
+//    public SelectPagedOrganizationsQueryHandler(IDatabase db)
+//    {
+//        _db = db;
+//    }
+
+//    public Page<Organization> Handle(SelectPagedOrganizationsQuery message)
+//    {
+//        Page<Organization> organizations = _db.Page<Organization>(message.PageIndex, message.PageSize, new Sql("SELECT o.* FROM \"Organizations\" o ORDER BY o.\"Guid\" DESC"));
+
+//        return organizations;
+//    }
+//}
