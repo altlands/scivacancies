df129a6557feb43b3bbfb06b0d164849ef0731cc
ntemnikov <ntemnikov@alt-lan.com>
2015-06-25 09:04:52 +0300

    vacancies displaying changes

---
+++ src/SciVacancies.WebApp/Controllers/PositionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace SciVacancies.WebApp.Controllers
+{
+    public class PositionsController: Controller
+    {
+        public IActionResult Details() => View();
+        public IActionResult Edit() => View();
+        public IActionResult Delete() => View();
+        public IActionResult Pyblish() => View();
+    }
+}
---
+++ src/SciVacancies.WebApp/Engine/All EnumHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using SciVacancies.Domain.Enums;
+
+namespace SciVacancies.WebApp
+{
+
+    public static class EnumExtenions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var type = value.GetType();
+            var name = Enum.GetName(type, value);
+
+            if (name == null) return null;
+
+            var field = type.GetField(name);
+
+            if (field == null) return null;
+
+            var attr = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+
+            return attr?.Description;
+        }
+
+        public static string GetHtmlClass(this VacancyStatus value)
+        {
+            switch (value)
+            {
+                case VacancyStatus.Cancelled:
+                    return "failed";
+                case VacancyStatus.Closed:
+                case VacancyStatus.Published:
+                case VacancyStatus.InCommittee:
+                    return "executed";
+                case VacancyStatus.AppliesAcceptance:
+                    return "work";
+                default: return null;
+            }
+        }
+
+        public static string GetHtmlClass(this PositionStatus value)
+        {
+            switch (value)
+            {
+                case PositionStatus.Removed:
+                case PositionStatus.InProcess:
+                    return "failed"; 
+                case PositionStatus.Published:
+                    return "executed";
+                default:
+                    return null;
+            }
+        }
+    }
+}
