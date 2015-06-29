3dc8fa6266d9bbd24d016b10d9285e40ede000b5
SashaOzz <sashaozz@outlook.com>
2015-06-29 17:24:59 +0300

    fixed enum conversion

--- src/SciVacancies.PostgresqlDB/update_amuravyev.bat
+++ src/SciVacancies.PostgresqlDB/update_amuravyev.bat
@@ -1,5 +1,7 @@
+pushd %~dp0
 set /p input=DropDatabase before applying migrations? [y/n] (default - n)
 
 if "%input%" EQU "y" (rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=postgres" -vf version.txt -env=AMURAVYEV /drop)
 
 rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=postgres" -vf version.txt -env=AMURAVYEV
+popd
