aa05cdf16bb5b9df258128d2019aaf97759a71f4
SashaOzz <sashaozz@outlook.com>
2015-06-29 05:24:03 +0300

    build

--- build/buildtools/build.bat
+++ build/buildtools/build.bat
@@ -1,1 +1,2 @@
 dnu publish "..\src\SciVacancies.WebApp" --out "build\SciVacancies.WebApp" --configuration Release --runtime dnx-clr-win-x64.1.0.0-beta4 --wwwroot-out "wwwroot" --quiet
+dnu publish "..\src\SciVacancies.PostgresqlDB" --out "build\SciVacancies.PostgresqlDB" --configuration Release --runtime dnx-clr-win-x64.1.0.0-beta4 --quiet
