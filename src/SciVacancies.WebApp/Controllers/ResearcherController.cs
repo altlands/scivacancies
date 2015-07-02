db964e1f55f0bcc88c4702bf897acab1d76f17aa
ADAvdeev <aavdeev@alt-lan.com>
2015-07-02 09:15:58 +0300

    "Thinktecture.IdentityModel": "3.6.1",         "Thinktecture.IdentityModel.Client": "4.0.1"

--- src/SciVacancies.WebApp/Controllers/ResearcherController.cs
+++ src/SciVacancies.WebApp/Controllers/ResearcherController.cs
@@ -4,6 +4,14 @@ using Nest;
 using SciVacancies.ReadModel.Core;
 using SciVacancies.WebApp.Queries;
 
+using Newtonsoft.Json.Linq;
+using System;
+//using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+//using System.Web.Mvc;
+using Thinktecture.IdentityModel.Client;
+
 namespace SciVacancies.WebApp.Controllers
 {
     public class ResearcherController : Controller
@@ -16,11 +24,59 @@ namespace SciVacancies.WebApp.Controllers
             _elastic = elastic;
             _mediator = mediator;
         }
+
+        Uri authEndpoint = new Uri("http://www.sciencemon.ru/oauth/v2/auth");
+        Uri tokenEndpoint = new Uri("http://www.sciencemon.ru/oauth/v2/token");
+        string clientId = "1_ikniwc909y8kog4k4sc00oogg0g8scc8o4k0wocgw4cg84k00";
+        string clientSecret = "67sq61c6xekgw0wgkosg04gwo488osk48ogks4og40cgws8ook";
+        Uri redirectUrl = new Uri("http://localhost:59075/researcher/callback");
+        //Uri redirectUrl = new Uri("http://localhost:59075");
+        string scope = null;
+
         // GET: /<controller>/
-        public void Index()
+        public ActionResult Index(string id)
         {
-            var pagedOrganizations = _mediator.Send(new SelectPagedOrganizationsQuery { PageIndex = 1, PageSize = 10 });
-            var sd = _elastic.Search<Vacancy>(s => s.Index("scivacancies"));
+
+            var client = new OAuth2Client(authEndpoint);
+
+            var s = client.CreateCodeFlowUrl(clientId, scope, redirectUrl.AbsoluteUri);
+
+            return Redirect(s);
+        }
+
+        public void Callback(string code)
+        {
+            var client = new OAuth2Client(tokenEndpoint);
+
+            //var s = client.RequestAuthorizationCodeAsync(code,)
+
+            var r = 0;
+
+        }
+        //private async Task<>
+
+
+
+        private async Task<TokenResponse> GetTokenAsync()
+        {
+            var client = new OAuth2Client(new Uri("http://www.sciencemon.ru/oauth/v2/token"), "1_ikniwc909y8kog4k4sc00oogg0g8scc8o4k0wocgw4cg84k00", "67sq61c6xekgw0wgkosg04gwo488osk48ogks4og40cgws8ook");
+            //var client = new OAuth2Client(new Uri("http://www.sciencemon.ru/oauth/v2/auth"), "1_ikniwc909y8kog4k4sc00oogg0g8scc8o4k0wocgw4cg84k00", "67sq61c6xekgw0wgkosg04gwo488osk48ogks4og40cgws8ook");
+
+            //var s=client.CreateCodeFlowUrl()
+
+            return await client.RequestClientCredentialsAsync();
+        }
+
+        private async Task<string> CallApi(string token)
+        {
+            var client = new System.Net.Http.HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            //var response =  client.GetAsync(new Uri("http://www.sciencemon.ru/oauth/v2/auth")).Result;
+            var response = client.GetAsync(new Uri("http://www.sciencemon.ru/api/user")).Result;
+
+            return response.Content.ReadAsStringAsync().Result;
         }
     }
 }
