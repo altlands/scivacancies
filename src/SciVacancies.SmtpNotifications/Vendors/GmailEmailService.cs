c5c1aab9075433e8e7041815df3f3471a349d473
ntemnikov <ntemnikov@alt-lan.com>
2015-07-02 15:00:51 +0300

    Initial classes for send Email

---
+++ src/SciVacancies.SmtpNotifications/Vendors/GmailEmailService.cs
@@ -0,0 +1,10 @@
+﻿namespace SciVacancies.SmtpNotifications.Vendors
+{
+    public class GmailEmailService : SmtpEmailService
+    {
+        public GmailEmailService(string login, string password)
+            : base(login, password, "smtp.gmail.com", 587, true, false)
+        {
+        }
+    }
+}
