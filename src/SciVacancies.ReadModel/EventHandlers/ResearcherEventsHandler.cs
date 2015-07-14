a5e5bde3b44f765c2e5d6a2cee519caca003b8ed
ADAvdeev <aavdeev@alt-lan.com>
2015-07-14 18:38:39 +0300

    RM handlers todo

--- src/SciVacancies.ReadModel/EventHandlers/ResearcherEventsHandler.cs
+++ src/SciVacancies.ReadModel/EventHandlers/ResearcherEventsHandler.cs
@@ -52,6 +52,7 @@ namespace SciVacancies.ReadModel.EventHandlers
             {
 
                 _db.Update(updatedResearcher);
+                //TODO - сделать без удаления неизменённых записей
                 _db.Execute(new Sql($"DELETE FROM res_educations WHERE researcher_guid = @0", msg.ResearcherGuid));
                 _db.Execute(new Sql($"DELETE FROM res_publications WHERE researcher_guid = @0", msg.ResearcherGuid));
                 foreach (Education ed in updatedResearcher.educations)
