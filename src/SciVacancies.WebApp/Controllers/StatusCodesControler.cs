0c14bfa0ffd1fd40fc6ce554df1332dd6d02e7db
ntemnikov <ntemnikov@alt-lan.com>
2015-07-22 11:05:36 +0300

    solved problem to show custom view in 404, 401, 500 etc. redirects

---
+++ src/SciVacancies.WebApp/Controllers/StatusCodesControler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace SciVacancies.WebApp.Controllers
+{
+    public class StatusCodesController : Controller
+    {
+        public IActionResult StatusCode404()
+        {
+            return Content("объект не найден");
+            //return View(viewName: "NotFound"); // you have a view called NotFound.cshtml
+        }
+
+        public IActionResult StatusCode401()
+        {
+            return Content("вы не авторизованы");
+            //return View(viewName: "NotFound"); // you have a view called NotFound.cshtml
+        }
+
+        //... more actions here to handle other status codes
+    }
+}
