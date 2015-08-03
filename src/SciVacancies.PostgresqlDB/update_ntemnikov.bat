0fb514901d43f7e8ce6c261133d2fec7bba7b67e
Nikita Temnikov <ntemnikov@alt-lan.com>
2015-08-03 12:35:48 +0300

    *.bat's modified

--- src/SciVacancies.PostgresqlDB/update_ntemnikov.bat
+++ src/SciVacancies.PostgresqlDB/update_ntemnikov.bat
@@ -1,8 +1,8 @@
 :menu
 @set /p input=DropDatabase before applying migrations? [y/n] (default - n)
 
-@if "%input%" EQU "y" (rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=postgres" -vf version.txt -env=NTEMNIKOV /drop)
+@if "%input%" EQU "y" (rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=postgres" -vf version.txt -env=NTEMNIKOV -silent /drop)
 
-@rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=postgres" -vf version.txt -env=NTEMNIKOV
+@rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=postgres" -vf version.txt -env=NTEMNIKOV -silent
 
 @goto menu
