94a322ebd12e9b472b496ea66d04482863fff330
Nikita Temnikov <ntemnikov@alt-lan.com>
2015-09-10 17:22:10 +0300

    defined SearchSubscriptions threads

--- src/SciVacancies.SearchSubscriptionsService/Jobs/SearchSubscriptionJob.cs
+++ src/SciVacancies.SearchSubscriptionsService/Jobs/SearchSubscriptionJob.cs
@@ -1,23 +1,23 @@
-﻿using SciVacancies.Domain.Enums;
-using SciVacancies.ReadModel.ElasticSearchModel.Model;
+﻿using System;
 using SciVacancies.SmtpNotifications;
-
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Nest;
 using Quartz;
 using NPoco;
 using Npgsql;
-using Nest;
 using Newtonsoft.Json;
+using SciVacancies.Domain.Enums;
 
-namespace SciVacancies.SearchSubscriptionsService
+namespace SciVacancies.SearchSubscriptionsService.Jobs
 {
     public class SearchSubscriptionJob : IJob
     {
-        static readonly string ServiceId="#1";
-        static readonly int Seed=1;
+        static readonly string ServiceId = "#1";
+        static readonly int Seed = 1;
 
         readonly IEmailService _emailService;
         //readonly IElasticService _elasticService;
@@ -31,134 +31,41 @@ namespace SciVacancies.SearchSubscriptionsService
             _db = db;
         }
 
+        /// <summary>
+        /// выполнить работу
+        /// </summary>
+        /// <param name="context"></param>
         public void Execute(IJobExecutionContext context)
         {
-            //    var dataBase = new Database("Server = localhost; Database = scivacancies; User Id = postgres; Password = postgres", NpgsqlFactory.Instance);
-            //    var elasticClient = new ElasticClient(new ConnectionSettings(new Uri("http://localhost:9200/"), defaultIndex: "scivacancies"));
+            ////
+            var dataBase = new Database("Server = localhost; Database = scivacancies; User Id = postgres; Password = postgres", NpgsqlFactory.Instance);
+            
 
-            //    //TODO - загружать по частям
-            //    Queue<SciVacancies.ReadModel.Core.SearchSubscription> subscriptionQueue = new Queue<ReadModel.Core.SearchSubscription>(dataBase.Fetch<SciVacancies.ReadModel.Core.SearchSubscription>(new Sql($"SELECT * FROM res_searchsubscriptions ss WHERE ss.status = @0", SearchSubscriptionStatus.Active)));
+            //TODO - загружать по частям, вызывая хранимую процедуру
+            Queue<SciVacancies.ReadModel.Core.SearchSubscription> subscriptionQueue = new Queue<ReadModel.Core.SearchSubscription>(dataBase.Fetch<SciVacancies.ReadModel.Core.SearchSubscription>(new Sql($"SELECT * FROM res_searchsubscriptions ss WHERE ss.status = @0", SearchSubscriptionStatus.Active)));
 
-            //    //int emailsToSentPerMinute
-            //    while (subscriptionQueue.Count > 0)
-            //    {
-            //        var searchSubscription = subscriptionQueue.Dequeue();
-            //        //TODO
+            const int poolCount = 20;
+            var threadSize = subscriptionQueue.Count > poolCount
+                ? (subscriptionQueue.Count / poolCount) + 1
+                : 1;
+            var manualResetEventsArray = new ManualResetEvent[poolCount];
+            var searchSubscriptionScannerArray = new SearchSubscriptionScanner[poolCount];
 
-            //        var searchQuery = new SearchQuery
-            //        {
-            //            Query = searchSubscription.query,
-            //            PublishDateFrom = searchSubscription.currentcheck_date,
-            //            FoivIds = JsonConvert.DeserializeObject<IEnumerable<int>>(searchSubscription.foiv_ids),
-            //            PositionTypeIds = JsonConvert.DeserializeObject<IEnumerable<int>>(searchSubscription.positiontype_ids),
-            //            RegionIds = JsonConvert.DeserializeObject<IEnumerable<int>>(searchSubscription.region_ids),
-            //            ResearchDirectionIds = JsonConvert.DeserializeObject<IEnumerable<int>>(searchSubscription.researchdirection_ids),
-            //            SalaryFrom = searchSubscription.salary_from,
-            //            SalaryTo = searchSubscription.salary_to,
-            //            VacancyStatuses = JsonConvert.DeserializeObject<IEnumerable<VacancyStatus>>(searchSubscription.vacancy_statuses)
-            //        };
+            var i = 0;
+            while (subscriptionQueue.Skip(threadSize * i).Take(threadSize).Any())
+            {
+                manualResetEventsArray[i] = new ManualResetEvent(false);
+                searchSubscriptionScannerArray[i] = new SearchSubscriptionScanner(manualResetEventsArray[i], subscriptionQueue.Skip(threadSize * i).Take(threadSize));
+                //todo ntemnikov parallel linq
+                ThreadPool.QueueUserWorkItem(searchSubscriptionScannerArray[i].PoolHandleSubscriptions);
+                manualResetEventsArray[i].Set();
+                i++;
+            }
 
-            //        Func<SearchQuery, SearchDescriptor<Vacancy>> searchSelector = VacancySearchDescriptor;
+            // Wait for all threads in pool to finish scanning.
+            WaitHandle.WaitAll(manualResetEventsArray);
 
-            //        var searchResults = elasticClient.Search<Vacancy>(searchSelector(searchQuery));
-            //        var vacanciesList = searchResults.Documents.ToList();
-
-            //        //if (vacanciesList.Count > 0)
-            //        //{
-            //        //    using (var transaction = dataBase.GetTransaction())
-            //        //    {
-            //        //        dataBase.Execute(new Sql("UPDATE res_searchsubscriptions SET currenttotal_count = @0, currentcheck_date = @1, lasttotal_count = @2, lastcheck_date = @3 WHERE guid = @4", vacanciesList.Count, DateTime.UtcNow, searchSubscription.currenttotal_count, searchSubscription.currentcheck_date, searchSubscription.guid));
-            //        //        transaction.Complete();
-            //        //    }
-
-            //        //    var researcher = dataBase.SingleOrDefaultById<SciVacancies.ReadModel.Core.Researcher>(searchSubscription.researcher_guid);
-            //        //    winnerSetSmtpNotificator.Send(searchSubscription, researcher, vacanciesList);
-
-            //        //}
-            //    }
-            //}
-
-            //public class SearchQuery
-            //{
-            //    public string Query { get; set; }
-
-            //    public long? PageSize { get; set; }
-            //    public long? CurrentPage { get; set; }
-
-            //    public string OrderFieldByDirection { get; set; }
-
-            //    public DateTime? PublishDateFrom { get; set; }
-
-            //    public IEnumerable<int> FoivIds { get; set; }
-            //    public IEnumerable<int> PositionTypeIds { get; set; }
-            //    public IEnumerable<int> RegionIds { get; set; }
-            //    public IEnumerable<int> ResearchDirectionIds { get; set; }
-
-            //    public int? SalaryFrom { get; set; }
-            //    public int? SalaryTo { get; set; }
-            //    public IEnumerable<VacancyStatus> VacancyStatuses { get; set; }
-            //}
-
-            //#region QueryContainers
-
-            //public SearchDescriptor<Vacancy> VacancySearchDescriptor(SearchQuery sq)
-            //{
-            //    SearchDescriptor<Vacancy> sdescriptor = new SearchDescriptor<Vacancy>();
-
-            //    if (sq.PageSize.HasValue && sq.CurrentPage.HasValue &&
-            //        sq.PageSize.Value != 0 && sq.CurrentPage.Value != 0)
-            //    {
-            //        sdescriptor.Skip((int)((sq.CurrentPage - 1) * sq.PageSize));
-            //        sdescriptor.Take((int)sq.PageSize);
-            //    }
-
-            //    sdescriptor.Sort(sort => sort.OnField(of => of.PublishDate).Descending());
-
-            //    Func<SearchQuery, QueryContainer> querySelector = VacancyQueryContainer;
-
-            //    sdescriptor.Query(querySelector(sq));
-
-            //    return sdescriptor;
-            //}
-            //public QueryContainer VacancyQueryContainer(SearchQuery sq)
-            //{
-
-            //    QueryContainer query = Query<Vacancy>.Filtered(fltd => fltd
-            //                                                .Query(q => q
-            //                                                    .FuzzyLikeThis(flt => flt
-            //                                                        .LikeText(sq.Query)
-            //                                                    )
-            //                                                )
-            //                                                .Filter(f => f
-            //                                                    .Terms<int>(ft => ft.OrganizationFoivId, sq.FoivIds)
-            //                                                    && f.Terms<int>(ft => ft.PositionTypeId, sq.PositionTypeIds)
-            //                                                    && f.Terms<int>(ft => ft.RegionId, sq.RegionIds)
-            //                                                    && f.Terms<int>(ft => ft.ResearchDirectionId, sq.ResearchDirectionIds)
-            //                                                    && f.Range(fr => fr
-            //                                                            .GreaterOrEquals((sq.SalaryFrom.HasValue && sq.SalaryFrom > 0) ? (long?)sq.SalaryFrom : null)
-            //                                                            .OnField(of => of.SalaryFrom)
-            //                                                    )
-            //                                                    && f.Range(fr => fr
-            //                                                            .LowerOrEquals((sq.SalaryTo.HasValue && sq.SalaryTo < 0) ? (long?)sq.SalaryTo : null)
-            //                                                            .OnField(of => of.SalaryTo)
-            //                                                    )
-            //                                                    && f.Terms<VacancyStatus>(ft => ft.Status, sq.VacancyStatuses)
-            //                                                    && f.Range(fr => fr
-            //                                                            .GreaterOrEquals(sq.PublishDateFrom)
-            //                                                            .OnField(of => of.PublishDate)
-            //                                                    )
-            //                                                    && f.Bool(b => b
-            //                                                            .MustNot(mn => mn
-            //                                                                .Terms<VacancyStatus>(mnt => mnt.Status, new List<VacancyStatus> { VacancyStatus.InProcess })
-            //                                                            )
-            //                                                    )
-            //                                                )
-            //                                        );
-
-            //    return query;
-            //}
-
-            //#endregion
         }
+
     }
 }
---
+++ src/SciVacancies.SearchSubscriptionsService/SearchSubscriptionScanner.cs
@@ -0,0 +1,167 @@
+﻿using Nest;
+using Newtonsoft.Json;
+using Npgsql;
+using NPoco;
+using Quartz;
+using SciVacancies.Domain.Enums;
+using SciVacancies.SmtpNotifications;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using SciVacancies.ReadModel.Core;
+
+namespace SciVacancies.SearchSubscriptionsService
+{
+    /// <summary>
+    /// выполнять обработку поискового запроса (запросов)
+    /// </summary>
+    public class SearchSubscriptionScanner
+    {
+        private readonly ManualResetEvent _doneEvent;
+        private readonly IEnumerable<SearchSubscription> _subscriptionQueue;
+
+        public SearchSubscriptionScanner(ManualResetEvent doneEvent, IEnumerable<SearchSubscription> subscriptionQueue)
+        {
+            _doneEvent = doneEvent;
+            _subscriptionQueue = subscriptionQueue;
+        }
+
+        /// <summary>
+        /// обработать несколько запросов
+        /// </summary>
+        public void PoolHandleSubscriptions(object threadContext)
+        {
+            if (_subscriptionQueue == null)
+                return;
+
+            var elasticClient = new ElasticClient(new ConnectionSettings(new Uri("http://localhost:9200/"), defaultIndex: "scivacancies"));
+
+            //todo int emailsToSentPerMinute
+            foreach (var searchSubscription in _subscriptionQueue)
+            {
+                //TODO
+
+                var searchQuery = new SearchQuery
+                {
+                    Query = searchSubscription.query,
+                    PublishDateFrom = searchSubscription.currentcheck_date,
+                    FoivIds = JsonConvert.DeserializeObject<IEnumerable<int>>(searchSubscription.foiv_ids),
+                    PositionTypeIds = JsonConvert.DeserializeObject<IEnumerable<int>>(searchSubscription.positiontype_ids),
+                    RegionIds = JsonConvert.DeserializeObject<IEnumerable<int>>(searchSubscription.region_ids),
+                    ResearchDirectionIds = JsonConvert.DeserializeObject<IEnumerable<int>>(searchSubscription.researchdirection_ids),
+                    SalaryFrom = searchSubscription.salary_from,
+                    SalaryTo = searchSubscription.salary_to,
+                    VacancyStatuses = JsonConvert.DeserializeObject<IEnumerable<VacancyStatus>>(searchSubscription.vacancy_statuses)
+                };
+
+                Func<SearchQuery, SearchDescriptor<SciVacancies.ReadModel.ElasticSearchModel.Model.Vacancy>> searchSelector = VacancySearchDescriptor;
+
+                var searchResults = elasticClient.Search<Vacancy>(searchSelector(searchQuery));
+                var vacanciesList = searchResults.Documents.ToList();
+
+                if (vacanciesList.Count > 0)
+                {
+                //    using (var transaction = dataBase.GetTransaction())
+                //    {
+                //        dataBase.Execute(new Sql("UPDATE res_searchsubscriptions SET currenttotal_count = @0, currentcheck_date = @1, lasttotal_count = @2, lastcheck_date = @3 WHERE guid = @4", vacanciesList.Count, DateTime.UtcNow, searchSubscription.currenttotal_count, searchSubscription.currentcheck_date, searchSubscription.guid));
+                //        transaction.Complete();
+                //    }
+
+                //    var researcher = dataBase.SingleOrDefaultById<SciVacancies.ReadModel.Core.Researcher>(searchSubscription.researcher_guid);
+                //    winnerSetSmtpNotificator.Send(searchSubscription, researcher, vacanciesList);
+
+                }
+            }
+
+            _doneEvent?.Set();
+        }
+
+
+        #region QueryContainers
+
+        public SearchDescriptor<SciVacancies.ReadModel.ElasticSearchModel.Model.Vacancy> VacancySearchDescriptor(SearchQuery sq)
+        {
+            var sdescriptor = new SearchDescriptor<SciVacancies.ReadModel.ElasticSearchModel.Model.Vacancy>();
+
+            if (sq.PageSize.HasValue && sq.CurrentPage.HasValue &&
+                sq.PageSize.Value != 0 && sq.CurrentPage.Value != 0)
+            {
+                sdescriptor.Skip((int)((sq.CurrentPage - 1) * sq.PageSize));
+                sdescriptor.Take((int)sq.PageSize);
+            }
+
+            sdescriptor.Sort(sort => sort.OnField(of => of.PublishDate).Descending());
+
+            Func<SearchQuery, QueryContainer> querySelector = VacancyQueryContainer;
+
+            sdescriptor.Query(querySelector(sq));
+
+            return sdescriptor;
+        }
+        public QueryContainer VacancyQueryContainer(SearchQuery sq)
+        {
+
+            var queryContainer = Query<SciVacancies.ReadModel.ElasticSearchModel.Model.Vacancy>.Filtered(fltd => fltd
+                     .Query(q => q
+                         .FuzzyLikeThis(flt => flt
+                             .LikeText(sq.Query)
+                         )
+                     )
+                     .Filter(f => f
+                         .Terms<int>(ft => ft.OrganizationFoivId, sq.FoivIds)
+                         && f.Terms<int>(ft => ft.PositionTypeId, sq.PositionTypeIds)
+                         && f.Terms<int>(ft => ft.RegionId, sq.RegionIds)
+                         && f.Terms<int>(ft => ft.ResearchDirectionId, sq.ResearchDirectionIds)
+                         && f.Range(fr => fr
+                                 .GreaterOrEquals((sq.SalaryFrom.HasValue && sq.SalaryFrom > 0) ? (long?)sq.SalaryFrom : null)
+                                 .OnField(of => of.SalaryFrom)
+                         )
+                         && f.Range(fr => fr
+                                 .LowerOrEquals((sq.SalaryTo.HasValue && sq.SalaryTo < 0) ? (long?)sq.SalaryTo : null)
+                                 .OnField(of => of.SalaryTo)
+                         )
+                         && f.Terms<VacancyStatus>(ft => ft.Status, sq.VacancyStatuses)
+                         && f.Range(fr => fr
+                                 .GreaterOrEquals(sq.PublishDateFrom)
+                                 .OnField(of => of.PublishDate)
+                         )
+                         && f.Bool(b => b
+                                 .MustNot(mn => mn
+                                     .Terms<VacancyStatus>(mnt => mnt.Status, new List<VacancyStatus> { VacancyStatus.InProcess })
+                                 )
+                         )
+                     )
+             );
+
+            return queryContainer;
+        }
+        #endregion
+
+
+
+        public class SearchQuery
+        {
+            public string Query { get; set; }
+
+            public long? PageSize { get; set; }
+            public long? CurrentPage { get; set; }
+
+            public string OrderFieldByDirection { get; set; }
+
+            public DateTime? PublishDateFrom { get; set; }
+
+            public IEnumerable<int> FoivIds { get; set; }
+            public IEnumerable<int> PositionTypeIds { get; set; }
+            public IEnumerable<int> RegionIds { get; set; }
+            public IEnumerable<int> ResearchDirectionIds { get; set; }
+
+            public int? SalaryFrom { get; set; }
+            public int? SalaryTo { get; set; }
+            public IEnumerable<VacancyStatus> VacancyStatuses { get; set; }
+        }
+
+    }
+}
