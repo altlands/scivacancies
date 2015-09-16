1c9c2ed6f04241a72f1314ecae2e5628730350ec
ADAvdeev <aavdeev@alt-lan.com>
2015-09-16 16:59:14 +0300

    DB fixes Saga fix

---
+++ src/SciVacancies.PostgresqlDB/up/001_21_committee_date.sql
@@ -0,0 +1,11 @@
+﻿SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+ALTER TABLE "org_vacancies" ADD COLUMN "committee_date" timestamp without time zone;
+
