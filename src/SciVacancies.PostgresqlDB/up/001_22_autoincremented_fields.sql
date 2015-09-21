8e4019953fc3b4d04924db5b517d0a103c73ac72
adavdeev <aavdeev@alt-lan.com>
2015-09-21 16:10:10 +0300

    DB update

---
+++ src/SciVacancies.PostgresqlDB/up/001_22_autoincremented_fields.sql
@@ -0,0 +1,16 @@
+﻿SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+CREATE SEQUENCE org_vacancy_id_seq;
+ALTER TABLE "org_vacancies" ADD COLUMN "id" integer NOT NULL default nextval('org_vacancy_id_seq');
+ALTER SEQUENCE org_vacancy_id_seq owned by org_vacancies.id;
+
+CREATE SEQUENCE res_vacancyapplications_id_seq;
+ALTER TABLE "res_vacancyapplications" ADD COLUMN "id" integer NOT NULL default nextval('res_vacancyapplications_id_seq');
+ALTER SEQUENCE res_vacancyapplications_id_seq owned by res_vacancyapplications.id;
