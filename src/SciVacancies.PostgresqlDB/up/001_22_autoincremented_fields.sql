2626b741bb9118d1cb22c3d298a4afcf35deb542
adavdeev <aavdeev@alt-lan.com>
2015-09-21 16:36:13 +0300

    id to read_id

--- src/SciVacancies.PostgresqlDB/up/001_22_autoincremented_fields.sql
+++ src/SciVacancies.PostgresqlDB/up/001_22_autoincremented_fields.sql
@@ -7,10 +7,10 @@ SET client_min_messages = warning;
 
 SET search_path = public, pg_catalog;
 
-CREATE SEQUENCE org_vacancy_id_seq;
-ALTER TABLE "org_vacancies" ADD COLUMN "id" bigint NOT NULL default nextval('org_vacancy_id_seq');
-ALTER SEQUENCE org_vacancy_id_seq owned by org_vacancies.id;
+CREATE SEQUENCE org_vacancy_read_id_seq;
+ALTER TABLE "org_vacancies" ADD COLUMN "read_id" bigint NOT NULL default nextval('org_vacancy_read_id_seq');
+ALTER SEQUENCE org_vacancy_read_id_seq owned by org_vacancies.read_id;
 
-CREATE SEQUENCE res_vacancyapplications_id_seq;
-ALTER TABLE "res_vacancyapplications" ADD COLUMN "id" bigint NOT NULL default nextval('res_vacancyapplications_id_seq');
-ALTER SEQUENCE res_vacancyapplications_id_seq owned by res_vacancyapplications.id;
+CREATE SEQUENCE res_vacancyapplications__read_id_seq;
+ALTER TABLE "res_vacancyapplications" ADD COLUMN "read_id" bigint NOT NULL default nextval('res_vacancyapplications_read_id_seq');
+ALTER SEQUENCE res_vacancyapplications_read_id_seq owned by res_vacancyapplications.read_id;
