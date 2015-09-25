bf78c28f82252e29f9c5af59d7e59b1f2ecb52e7
Nikita Temnikov <ntemnikov@alt-lan.com>
2015-09-25 15:28:34 +0300

    added ViewComponent: "VacancyApplicationInfo"

---
+++ src/SciVacancies.WebApp/ViewComponents/VacancyApplicationInfo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.OptionsModel;
+using SciVacancies.WebApp.ViewModels;
+
+namespace SciVacancies.WebApp.ViewComponents
+{
+    public class VacancyApplicationInfo: ViewComponent
+    {
+        private readonly IOptions<AttachmentSettings> _attachmentSettings;
+        public VacancyApplicationInfo(IOptions<AttachmentSettings> attachmentSettings)
+        {
+            _attachmentSettings = attachmentSettings;
+
+        }
+        public IViewComponentResult Invoke(VacancyApplicationDetailsViewModel model)
+        {
+            model.FolderApplicationsAttachmentsUrl= _attachmentSettings.Options.VacancyApplication.UrlPathPart;
+            return View("/Views/Partials/_VacancyApplicationInfo", model);
+        }
+    }
+}
