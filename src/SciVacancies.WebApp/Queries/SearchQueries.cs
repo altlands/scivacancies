416e65767dc5647c4cba23aa4a9f168091548030
ADAvdeev <aavdeev@alt-lan.com>
2015-10-06 10:00:38 +0300

    minor fix

--- src/SciVacancies.WebApp/Queries/SearchQueries.cs
+++ src/SciVacancies.WebApp/Queries/SearchQueries.cs
@@ -11,22 +11,6 @@ namespace SciVacancies.WebApp.Queries
 {
     public class SearchQuery : SciVacancies.Services.Elastic.SearchQuery, IRequest<Page<Vacancy>>
     {
-        //public string Query { get; set; }
 
-        //public long? PageSize { get; set; }
-        //public long? CurrentPage { get; set; }
-
-        //public string OrderFieldByDirection { get; set; }
-
-        //public DateTime? PublishDateFrom { get; set; }
-
-        //public IEnumerable<int> FoivIds { get; set; }
-        //public IEnumerable<int> PositionTypeIds { get; set; }
-        //public IEnumerable<int> RegionIds { get; set; }
-        //public IEnumerable<int> ResearchDirectionIds { get; set; }
-
-        //public int? SalaryFrom { get; set; }
-        //public int? SalaryTo { get; set; }
-        //public IEnumerable<VacancyStatus> VacancyStatuses { get; set; }
     }
 }
