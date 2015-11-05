4da0576aea918843b3e00ed66cd7ab726041c281
Nikita Temnikov <ntemnikov@alt-lan.com>
2015-11-05 16:25:04 +0300

    configs removed

--- src/SciVacancies.PostgresqlDB/update_ekobka.bat
+++
@@ -1,8 +0,0 @@
-:menu
-@set /p input=DropDatabase before applying migrations? [y/n] (default - n)
-
-@if "%input%" EQU "y" (rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=ekobka" -vf version.txt -env=EKOBKA -silent /drop)
-
-@rh.exe --databasetype=postgresql -c "Server=localhost;Database=scivacancies;User Id=postgres;Password=ekobka" -vf version.txt -env=EKOBKA -silent
-
-@goto menu
