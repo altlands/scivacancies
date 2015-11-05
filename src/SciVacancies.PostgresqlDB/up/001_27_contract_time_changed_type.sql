c2a1d6f335985c03e4db8e76bee212e687712a35
Nikita Temnikov <ntemnikov@alt-lan.com>
2015-11-05 17:28:20 +0300

    fixe: migration for contract_time

---
+++ src/SciVacancies.PostgresqlDB/up/001_27_contract_time_changed_type.sql
@@ -0,0 +1,13 @@
+﻿SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+ALTER TABLE org_vacancies ADD COLUMN contract_time_new  real;
+UPDATE org_vacancies t2 SET contract_time_new = t1.contract_time FROM org_vacancies t1 WHERE t2.guid = t1.guid;
+ALTER TABLE org_vacancies DROP COLUMN contract_time;
+ALTER TABLE org_vacancies RENAME COLUMN contract_time_new TO contract_time;
