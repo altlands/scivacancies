c8b7e5105a697add7a868ecbc015d4e84db35713
Nikita Temnikov <ntemnikov@alt-lan.com>
2015-11-11 10:55:46 +0300

    added empty Analytics controller

---
+++ src/SciVacancies.WebApp/Controllers/AnalyticsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SciVacancies.WebApp.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        /// <summary>
+        /// Число вакансий (среднее предложение в месяц)
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult VacancyAmountInPeriod()
+        {
+            List<int> model = new List<int>();
+
+            return Json(model);
+        }
+
+
+    }
+}
