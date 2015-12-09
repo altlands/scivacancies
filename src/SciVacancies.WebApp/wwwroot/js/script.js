4e061f95a22db0a2e49048e753af923d298d4362
Nikita Temnikov <ntemnikov@alt-lan.com>
2015-12-09 16:38:14 +0300

    fixed tiles text height on the Main page

--- src/SciVacancies.WebApp/wwwroot/js/script.js
+++ src/SciVacancies.WebApp/wwwroot/js/script.js
@@ -61,7 +61,7 @@ $(document).ready(function () {
         lastCharacter: {
 
             /*	Remove these characters from the end of the truncated text. */
-            remove: [' ', ',', ';', '.', '!', '?'],
+            remove: [' ', ',', ';', '.', '-', '!', '?'],
 
             /*	Don't add an ellipsis if this array contains 
 				the last character of the truncated text. */
@@ -97,7 +97,43 @@ $(document).ready(function () {
         lastCharacter: {
 
             /*	Remove these characters from the end of the truncated text. */
-            remove: [' ', ',', ';', '.', '!', '?'],
+            remove: [' ', ',', ';', '.', '-', '!', '?'],
+
+            /*	Don't add an ellipsis if this array contains 
+				the last character of the truncated text. */
+            noEllipsis: []
+        }
+    });
+    $(".line_five_content").dotdotdot({
+        /*	The text to add as ellipsis. */
+        ellipsis: '... ',
+
+        /*	How to cut off the text/html: 'word'/'letter'/'children' */
+        wrap: 'word',
+
+        /*	Wrap-option fallback to 'letter' for long words */
+        fallbackToLetter: true,
+
+        /*	jQuery-selector for the element to keep and put after the ellipsis. */
+        after: null,
+
+        /*	Whether to update the ellipsis: true/'window' */
+        watch: false,
+
+        /*	Optionally set a max-height, if null, the height will be measured. */
+        height: null,
+
+        /*	Deviation for the height-option. */
+        tolerance: 0,
+
+        /*	Callback function that is fired after the ellipsis is added,
+			receives two parameters: isTruncated(boolean), orgContent(string). */
+        callback: function (isTruncated, orgContent) { },
+
+        lastCharacter: {
+
+            /*	Remove these characters from the end of the truncated text. */
+            remove: [' ', ',', ';', '.', '-', '!', '?'],
 
             /*	Don't add an ellipsis if this array contains 
 				the last character of the truncated text. */
