690ef15e63251a9e369fe6b02b8132c24d78d3c1
Nikita Temnikov <ntemnikov@alt-lan.com>
2015-12-09 17:02:13 +0300

    Email Service can be using without Credentials

--- src/SciVacancies.Services/Email/EmailService.cs
+++ src/SciVacancies.Services/Email/EmailService.cs
@@ -1,60 +1,85 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.Framework.Logging;
 using Microsoft.Framework.Configuration;
+
 
 namespace SciVacancies.Services.Email
 {
     public class EmailService : IEmailService, IDisposable
     {
-        readonly IConfiguration Configuration;
+        public ILogger Logger { get; set; }
 
-        readonly Lazy<SmtpClient> SmtpClient;
+        private readonly IConfiguration _configuration;
+        private readonly Lazy<SmtpClient> _smtpClient;
 
-        public EmailService(IConfiguration configuration)
+        public EmailService(IConfiguration configuration, ILoggerFactory loggerFactory)
         {
-            this.Configuration = configuration;
 
-            if (string.IsNullOrWhiteSpace(Configuration["EmailSettings:Login"]))
-                throw new ArgumentNullException("Не указан логин для подключения к серверу рассылку email-уведомлений");
-            //if (string.IsNullOrWhiteSpace(Configuration["EmailSettings:Password"]))
-            //    throw new ArgumentNullException("Не указан пароль для подключения к серверу рассылку email-уведомлений");
+            _configuration = configuration;
+            Logger = loggerFactory.CreateLogger<EmailService>();
 
-            this.SmtpClient = new Lazy<SmtpClient>(() => new SmtpClient
+            Logger.LogInformation("_smtpClient configuration starting");
+            if (!string.IsNullOrWhiteSpace(_configuration["EmailSettings:Login"])
+                && !string.IsNullOrWhiteSpace(_configuration["EmailSettings:Password"])
+                && bool.Parse(_configuration["EmailSettings:UseDefaultCredentials"]))
             {
-                Host = Configuration["EmailSettings:Host"],
-                Port = int.Parse(Configuration["EmailSettings:Port"], CultureInfo.InvariantCulture),
-                EnableSsl = bool.Parse(Configuration["EmailSettings:EnableSSL"]),
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = bool.Parse(Configuration["EmailSettings:UseDefaultCredentials"]),
-                Credentials = new NetworkCredential(Configuration["EmailSettings:Login"], Configuration["EmailSettings:Password"])
-            });
+                _smtpClient = new Lazy<SmtpClient>(() =>
+                new SmtpClient
+                {
+                    Host = _configuration["EmailSettings:Host"],
+                    Port = int.Parse(_configuration["EmailSettings:Port"], CultureInfo.InvariantCulture),
+                    EnableSsl = bool.Parse(_configuration["EmailSettings:EnableSSL"]),
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = bool.Parse(_configuration["EmailSettings:UseDefaultCredentials"]),
+                    Credentials = new NetworkCredential(_configuration["EmailSettings:Login"], _configuration["EmailSettings:Password"])
+                });
+
+                Logger.LogInformation("_smtpClient configured to work with credentials");
+            }
+            else
+            {
+                _smtpClient = new Lazy<SmtpClient>(() =>
+                new SmtpClient
+                {
+                    Host = _configuration["EmailSettings:Host"],
+                    Port = int.Parse(_configuration["EmailSettings:Port"], CultureInfo.InvariantCulture),
+                    EnableSsl = bool.Parse(_configuration["EmailSettings:EnableSSL"]),
+                    DeliveryMethod = SmtpDeliveryMethod.Network
+                });
+
+                Logger.LogInformation("_smtpClient configured to work without credentials");
+            }
+
             //Неведомый костыль для работы с ssl сертификатами
-            ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-            { return true; };
+            if (bool.Parse(_configuration["EmailSettings:EnableSSL"]))
+                ServicePointManager.ServerCertificateValidationCallback = (s, certificate, chain, sslPolicyErrors) => true;
         }
 
         public virtual void Send(MailMessage message)
         {
             try
             {
-                SmtpClient.Value.Send(message);
+                Logger.LogInformation($"Attempt to send email to {message.To}");
+                _smtpClient.Value.Send(message);
+                Logger.LogInformation($"Email sended to {message.To}");
             }
             catch (Exception e)
             {
-                throw e;
-                //todo: логировать ошибки при отправке почты
+                Logger.LogError($"Email failed to send: {e.Message}", e);
             }
         }
 
         public void Dispose()
         {
-            if (SmtpClient.IsValueCreated)
+            if (_smtpClient.IsValueCreated)
             {
-                SmtpClient.Value.Dispose();
+                _smtpClient.Value.Dispose();
             }
         }
     }
