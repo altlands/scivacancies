9969322ad86e64b7bc0e0ee7f4a70730d9bbb2b1
Nikita Temnikov <ntemnikov@alt-lan.com>
2015-12-09 16:54:17 +0300

    fixed Password Restore errors

--- src/SciVacancies.WebApp/ViewModels/RestorePasswordViewModel.cs
+++ src/SciVacancies.WebApp/ViewModels/RestorePasswordViewModel.cs
@@ -12,12 +12,14 @@ namespace SciVacancies.WebApp.ViewModels
         [Required(ErrorMessage = "Требуется ввести пароль")]
         [PasswordPropertyText]
         [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Длинна пароля должна быть не менее 6 символов")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Требуется повторить пароль")]
         [PasswordPropertyText]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Пароли отличаются")]
+        [MinLength(6, ErrorMessage = "Длинна пароля должна быть не менее 6 символов")]
         public string PasswordConfirm { get; set; }
     }
 }
